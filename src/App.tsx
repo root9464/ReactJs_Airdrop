ff6074b405626f93b3a7e524e2a97ad96a9f0ef7
iO <senpai9464@yandex.ru>
2025-04-24 20:54:42 +0300

    upd: add button colorize

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Address, toNano } from '@ton/core';
-import { SendTransactionRequest, TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react';
-import { airdropAddress, codeHex, dictBase64 } from './shared/constants/constants';
+import { SendTransactionRequest, TonConnectButton, useTonAddress, useTonConnectUI } from '@tonconnect/ui-react';
+import { airdropAddress, codeHex, dictBase64, entries } from './shared/constants/constants';
 import { generateLimitOrderPayload } from './shared/helpers/helpers';
 
 export default function App() {
   const [tonConnectUi] = useTonConnectUI();
+  const myAddress = useTonAddress();
   const { address, stateInitBase64, msgBody } = generateLimitOrderPayload({
     airdropAddress: Address.parse(airdropAddress),
     indexFutureOwner: 5n,
@@ -28,13 +29,16 @@ export default function App() {
     tonConnectUi.sendTransaction(message);
   };
 
+  const isAddressInEntries = myAddress ? entries.some((entry) => entry.address === myAddress) : false;
+  const buttonColor = !myAddress ? 'bg-gray-500' : isAddressInEntries ? 'bg-green-500' : 'bg-red-500';
+
   return (
     <main className='flex h-screen w-full items-center justify-center'>
       <div className='flex h-[300px] w-[300px] flex-col items-center justify-center gap-5 rounded-lg bg-gray-700'>
         <TonConnectButton />
         <p className='text-center text-sm'>Get airdrop by connecting your wallet</p>
-        <button className='rounded-md bg-blue-500 p-2 text-white' onClick={claim}>
-          Get airdrop
+        <button className={`rounded-md p-2 text-white ${buttonColor}`} onClick={claim}>
+          {isAddressInEntries ? `Claimed ${entries.find((entry) => entry.address === myAddress)?.amount} Jettons` : 'You not claimed'}
         </button>
       </div>
     </main>
